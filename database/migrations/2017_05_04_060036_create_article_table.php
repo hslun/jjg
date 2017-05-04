089f23ac7d57d7c6a30b35cfb88e1d17a3f65479
housl <cuixin_427@163.com>
2017-05-04 14:21:43 +0800

    添加phpmyadmin，增加商品表

---
+++ database/migrations/2017_05_04_060036_create_article_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jjg_goods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('goods_name');
+            $table->float('goods_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jjg_goods');
+    }
+}
